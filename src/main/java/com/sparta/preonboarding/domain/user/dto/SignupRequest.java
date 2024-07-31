a700d4b89d65d4f2da4966384c3769d29069bf93
oyeonjoo <oyj0842@naver.com>
2024-07-31 13:48:31 +0900

    refactor: update SignupDto

--- src/main/java/com/sparta/preonboarding/domain/user/dto/SignupRequest.java
+++ src/main/java/com/sparta/preonboarding/domain/user/dto/SignupRequest.java
@@ -1,5 +1,6 @@
 package com.sparta.preonboarding.domain.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,4 +9,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignupRequest {
+
+    @NotBlank(message = "이름을 입력하세요.")
+    private String username;
+
+    @NotBlank(message = "비밀번호를 입력하세요")
+    private String password;
+
+    @NotBlank(message = "닉네임을 입력하세요.")
+    private String nickname;
 }
--- src/main/java/com/sparta/preonboarding/domain/user/dto/SignupResponse.java
+++ src/main/java/com/sparta/preonboarding/domain/user/dto/SignupResponse.java
@@ -1,5 +1,6 @@
 package com.sparta.preonboarding.domain.user.dto;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,4 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignupResponse {
+
+    private String username;
+    private String nickname;
+    private List<AuthorityDto> authorities;
 }
