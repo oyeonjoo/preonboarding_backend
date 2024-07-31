c00e81bb3e3cf09938f27cd3adc471b634684297
oyeonjoo <oyj0842@naver.com>
2024-07-31 17:55:18 +0900

    refactor: add @Getter

--- src/main/java/com/sparta/preonboarding/domain/user/dto/AuthorityDto.java
+++ src/main/java/com/sparta/preonboarding/domain/user/dto/AuthorityDto.java
@@ -1,10 +1,13 @@
 package com.sparta.preonboarding.domain.user.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthorityDto {
+
     private String authorityName;
 }
