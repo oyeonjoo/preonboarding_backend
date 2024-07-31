c567126db965a0ea65db367712f8014dfec62591
oyeonjoo <oyj0842@naver.com>
2024-07-31 22:18:34 +0900

    refactor: add ROLE_ADMIN and constructor

--- src/main/java/com/sparta/preonboarding/domain/user/entity/UserRole.java
+++ src/main/java/com/sparta/preonboarding/domain/user/entity/UserRole.java
@@ -4,11 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum UserRole {
-    USER("ROLE_USER");
+  USER("ROLE_USER"),
+  ADMIN("ROLE_ADMIN");
 
-    private final String authority;
+  private final String authority;
 
-    UserRole(String authority) {
-        this.authority = authority;
-    }
+  UserRole(String authority) {
+    this.authority = authority;
+  }
 }
