b51a1dd03c4a682fe3b416079f820b97679b56b6
oyeonjoo <oyj0842@naver.com>
2024-07-31 22:21:03 +0900

    feat: add ScheduleConfig

---
+++ src/main/java/com/sparta/preonboarding/global/config/schedule/ScheduleConfig.java
@@ -0,0 +1,9 @@
+package com.sparta.preonboarding.global.config.schedule;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class ScheduleConfig {
+}
