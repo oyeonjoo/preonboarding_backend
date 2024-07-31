206be77835deeff372f617b7bbfb843bf006eed1
oyeonjoo <oyj0842@naver.com>
2024-07-31 13:41:47 +0900

    feat: add AuthorityDto

---
+++ src/main/java/com/sparta/preonboarding/domain/user/dto/AuthorityDto.java
@@ -0,0 +1,10 @@
+package com.sparta.preonboarding.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorityDto {
+    private String authorityName;
+}
